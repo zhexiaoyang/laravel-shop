67b9c7f97960737ebdcc82bdf80509961a2f2ecf
vagrant <vagrant@homestead>
2018-12-14 15:23:05 +0000

    完善支付后逻辑

---
+++ app/Listeners/UpdateProductSoldCount.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPaid;
+use App\Models\OrderItem;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UpdateProductSoldCount
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderPaid  $event
+     * @return void
+     */
+    public function handle(OrderPaid $event)
+    {
+        $order = $event->getOrder();
+
+        $order->load(['items.product']);
+
+        foreach ($order->items as $item) {
+            $product = $item->product;
+            $soldCount = OrderItem::query()
+                ->where('product_id', $product->id)
+                ->whereHas('order', function ($query) {
+                    $query->whereNotNull('paid_at');  // 关联的订单状态是已支付
+                })->sum('amount');
+            // 更新商品销量
+            $product->update([
+                'sold_count' => $soldCount,
+            ]);
+        }
+    }
+}
