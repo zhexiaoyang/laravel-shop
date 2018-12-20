441ffd363941e20efb9742ba9aa355a7ecd2a7ef
vagrant <vagrant@homestead>
2018-12-20 15:54:12 +0000

    后台订单

---
+++ app/Admin/Controllers/OrdersController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Exceptions\InvalidRequestException;
+use App\Models\Order;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('订单列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show(Order $order, Content $content)
+    {
+        return $content
+            ->header('查看订单')
+            ->body(view('admin.orders.show', ['order' => $order]));
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Order);
+
+        // 只展示已支付的订单，并且默认按支付时间倒序排序
+        $grid->model()->whereNotNull('paid_at')->orderBy('paid_at', 'desc');
+
+        $grid->no('订单流水号');
+        // 展示关联关系的字段时，使用 column 方法
+        $grid->column('user.name', '买家');
+        $grid->total_amount('总金额')->sortable();
+        $grid->paid_at('支付时间')->sortable();
+        $grid->ship_status('物流')->display(function($value) {
+            return Order::$shipStatusMap[$value];
+        });
+        $grid->refund_status('退款状态')->display(function($value) {
+            return Order::$refundStatusMap[$value];
+        });
+        // 禁用创建按钮，后台不需要创建订单
+        $grid->disableCreateButton();
+        $grid->actions(function ($actions) {
+            // 禁用删除和编辑按钮
+            $actions->disableDelete();
+            $actions->disableEdit();
+        });
+        $grid->tools(function ($tools) {
+            // 禁用批量删除按钮
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+
+        return $grid;
+    }
+
+    public function ship(Order $order, Request $request)
+    {
+        // 判断当前订单是否已支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未付款');
+        }
+        // 判断当前订单发货状态是否为未发货
+        if ($order->ship_status !== Order::SHIP_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已发货');
+        }
+        // Laravel 5.5 之后 validate 方法可以返回校验过的值
+        $data = $this->validate($request, [
+            'express_company' => ['required'],
+            'express_no'      => ['required'],
+        ], [], [
+            'express_company' => '物流公司',
+            'express_no'      => '物流单号',
+        ]);
+        // 将订单发货状态改为已发货，并存入物流信息
+        $order->update([
+            'ship_status' => Order::SHIP_STATUS_DELIVERED,
+            // 我们在 Order 模型的 $casts 属性里指明了 ship_data 是一个数组
+            // 因此这里可以直接把数组传过去
+            'ship_data'   => $data,
+        ]);
+
+        // 返回上一页
+        return redirect()->back();
+    }
+}
